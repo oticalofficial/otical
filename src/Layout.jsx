bfbc5a64453279c3663f328c25e1d3799cbf7c05
Puneet Kumar <Puneetshivaay@gmail.com>
2025-04-09 04:35:39 +0000

    FooterBottom Modified

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,6 +14,7 @@ function Layout() {
     <Outlet />
     <Footer />
     <FooterBottom />
+
     </div>
     </>
   )
--- src/components/Footer/FooterBottom.jsx
+++ src/components/Footer/FooterBottom.jsx
@@ -4,42 +4,12 @@ const FooterBottom = () => {
     return (
         <footer className="mt-20 border-t py-10 border-neutral-700">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-
-                <div className="md:flex md:justify-between">
-                    <div className="mb-6 md:mb-0">
-                        <a href="https://otical.in/" className="flex items-center">
-                            <img className="h-10 w-10 mr-2" src={logo} alt="Otical Logo" />
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Otical</span>
-                        </a>
-                    </div>
-                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                    <div>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                                <li>
-                                    <a href="https://otical.in/" className="hover:underline"></a>
-                                </li>
-                            </ul>
-                        </div>
- 
-                        <div>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                                <li>
-                                    <a href="https://otical.in/" className="hover:underline">Terms &amp; Conditions</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                                <li className="mb-4">
-                                    <a href="https://otical.in/" className="hover:underline">Privacy Policy</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://otical.in/" className="hover:underline">Otical™</a>. All Rights Reserved.
+                    <a href="https://otical.in/" className="flex items-center">
+                        <img className="h-10 w-10 mr-2" src={logo} alt="Otical Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Otical</span>
+                    </a>
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://otical.in/" className="hover:underline">Otical™</a> All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <a href="https://x.com/oticalofficial" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
